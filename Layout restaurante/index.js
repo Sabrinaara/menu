8203217c364586bbe765ecd59d65ba03104d1602
Sabrina Araújo <82546575+Sabrinaara@users.noreply.github.com>
2023-09-15 12:22:23 -0300

    Add files via upload

---
+++ Layout restaurante/index.js
@@ -0,0 +1,23 @@
+
+
+var btn = document.querySelector("#back-to-top");
+
+btn.addEventListener("click", function(){
+    window.scrollTo(0,0)
+})
+
+function myFunction(){
+    var x = document.getElementById("myLinks")
+   
+    if(x.style.display === "block"){
+
+        x.style.display = "none";
+        
+
+    }else{
+        x.style.display = "block"
+    }
+    console.log(x, "clico")
+}
+
+
